5caca65fbd058513a84f921ece0e592c7d32553f
rbrinson2 <48216680+rbrinson2@users.noreply.github.com>
2025-02-10 14:32:05 -0500

    Task 3 and lab complete

--- Lab3/Core/Src/main.c
+++ Lab3/Core/Src/main.c
@@ -32,10 +32,11 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define N 20
+#define N 41
 #define a 2.2
 #define b -1.1
 #define c 0.7
+#define FILTER_SIZE 15
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -47,7 +48,30 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+float sine1[N], sineComposite[N], sineFiltered[N];
+static float filter[FILTER_SIZE] = {     // Discrete time low pass filter which filters above 20 Hz
+    -0.01259277,
+    -0.02704833,
+    -0.03115702,
+    -0.00335167,
+    0.06651710,
+    0.16356430,
+    0.24972947,
+    0.28427791,
+    0.24972947,
+    0.16356430,
+    0.06651710,
+    -0.00335167,
+    -0.03115702,
+    -0.02704833,
+    -0.01259277
+};
 
+float f1 = 5;   // Sine Frequency 1
+float f2 = 25;  // Sine Frequency 2
+float f3 = 35;  // Sine Frequency 3
+float f4 = 45;  // Sine Frequency 4
+float fs = 100; // Sampling Frequency
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -67,6 +91,29 @@ void system1(float input[], float output[], int size){
     else output[n] = input[n] + b * input[n - 1]; 
   }
 }
+
+void convolution(float x[], float h[], float y[], int sizeX, int sizeH)
+{
+  int n, k;  // variables for loops
+  int p=0;    // index variable for output signal y
+  float sum;  // convolutions sum accumulator variable
+  int Low_lim = sizeH/2;      // Staring point of the center pick N elements of the output array
+  int L = sizeX + sizeH - 1;  // Original length of the convolution.
+
+  for (n = 0; n < L; n++) {
+    sum = 0;
+    for (k = 0; k < sizeX; k++) {
+      if ((n - k) >= 0 && (n - k) < sizeH) {
+          sum = sum + x[k]*h[n-k];   // Main convolution sum calculation
+      }
+    }
+    if(n>=Low_lim && n<(Low_lim+N))  // Only pick center N elements of the output array!
+    {
+      y[p] = sum;
+      p++;
+    }
+  }
+}
 /* USER CODE END 0 */
 
 /**
@@ -83,7 +130,14 @@ int main(void)
   }
   /* USER CODE BEGIN 1 */
   system1(inp, outp, N);
-
+    //Single Tone Sinusoidal signal and Four Frequency Combined Sinusoidal Signal
+  for(int n = 0; n < N; n++)
+  {
+      sine1[n]=sin(2*PI*f1/fs*(float)n);
+      sineComposite[n]=sine1[n]+sin(2*PI*f2/fs*(float)n)+sin(2*PI*f3/fs*(float)n)+sin(2*PI*f4/fs*(float)n);
+  }
+  // Call Convolution Function to filter Combined Signal
+  convolution(sineComposite, filter, sineFiltered, N, FILTER_SIZE);
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
