af41efaa6001f9ce78460e710402cab49ead9954
rbrinson2 <48216680+rbrinson2@users.noreply.github.com>
2025-02-17 13:14:42 -0500

    Configured ADC

--- Lab4/Core/Src/main.c
+++ Lab4/Core/Src/main.c
@@ -32,11 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define N 41
-#define a 2.2
-#define b -1.1
-#define c 0.7
-#define FILTER_SIZE 15
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -45,82 +41,26 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-float sine1[N], sineComposite[N], sineFiltered[N];
-static float filter[FILTER_SIZE] = {     // Discrete time low pass filter which filters above 20 Hz
-    -0.01259277,
-    -0.02704833,
-    -0.03115702,
-    -0.00335167,
-    0.06651710,
-    0.16356430,
-    0.24972947,
-    0.28427791,
-    0.24972947,
-    0.16356430,
-    0.06651710,
-    -0.00335167,
-    -0.03115702,
-    -0.02704833,
-    -0.01259277
-};
 
-float f1 = 5;   // Sine Frequency 1
-float f2 = 25;  // Sine Frequency 2
-float f3 = 35;  // Sine Frequency 3
-float f4 = 45;  // Sine Frequency 4
-float fs = 100; // Sampling Frequency
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void system1(float input[], float output[], int size){
-  int n;
-  for (n = 0; n < size; n++) {
-    if (n == 0) output[n] = input[n];
-    else output[n] = input[n] + b * input[n - 1]; 
-  }
-}
 
-void system2(float input[], float output[], int size){
-  int n;
-  for (n = 0; n < size; n++) {
-    if (n == 0) output[n] = input[n];
-    else output[n] = a * input[n] + b * input[n - 1] + c * output[n - 1]; 
-  }
-}
-void convolution(float x[], float h[], float y[], int sizeX, int sizeH)
-{
-  int n, k;  // variables for loops
-  int p=0;    // index variable for output signal y
-  float sum;  // convolutions sum accumulator variable
-  int Low_lim = sizeH/2;      // Staring point of the center pick N elements of the output array
-  int L = sizeX + sizeH - 1;  // Original length of the convolution.
-
-  for (n = 0; n < L; n++) {
-    sum = 0;
-    for (k = 0; k < sizeX; k++) {
-      if ((n - k) >= 0 && (n - k) < sizeH) {
-          sum = sum + x[k]*h[n-k];   // Main convolution sum calculation
-      }
-    }
-    if(n>=Low_lim && n<(Low_lim+N))  // Only pick center N elements of the output array!
-    {
-      y[p] = sum;
-      p++;
-    }
-  }
-}
 /* USER CODE END 0 */
 
 /**
@@ -129,23 +69,9 @@ void convolution(float x[], float h[], float y[], int sizeX, int sizeH)
   */
 int main(void)
 {
-  float inp[N];
-  float outp[N];
-  for (int i = 0; i < N; i++) {
-    if (i < 7) inp[i] = 1;
-    else inp[i] = 0;
-  }
+
   /* USER CODE BEGIN 1 */
-  // system1(inp, outp, N);
-  system2(inp, outp, N);
-    //Single Tone Sinusoidal signal and Four Frequency Combined Sinusoidal Signal
-  for(int n = 0; n < N; n++)
-  {
-      sine1[n]=sin(2*PI*f1/fs*(float)n);
-      sineComposite[n]=sine1[n]+sin(2*PI*f2/fs*(float)n)+sin(2*PI*f3/fs*(float)n)+sin(2*PI*f4/fs*(float)n);
-  }
-  // Call Convolution Function to filter Combined Signal
-  convolution(sineComposite, filter, sineFiltered, N, FILTER_SIZE);
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -167,6 +93,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -229,6 +156,73 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_MultiModeTypeDef multimode = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc1.Init.OversamplingMode = DISABLE;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the ADC multi-mode
+  */
+  multimode.Mode = ADC_MODE_INDEPENDENT;
+  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_5;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
